6654a7b747489c53fb9eacd93b96a5ae9d61394c
dmitry.oleinik <dmitry.oleinik@rhinocodes.com>
2022-12-28 09:08:07 +0200

    init

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-let ip = require("ip");
+// let ip = require("ip");
 
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
@@ -11,14 +11,20 @@ const webAppUrl = 'https://splendorous-buttercream-c2243b.netlify.app';
 const bot = new TelegramBot(token, {polling: true});
 const app = express();
 
+const admin_id = '350664322';
+let user_id = '';
+let message = '';
+
 app.use(express.json());
 app.use(cors());
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
+    message = msg.text;
+    user_id = msg.chat.id;
 
-    if(text === '/start') {
+    if (text === '/start') {
         await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
             reply_markup: {
                 keyboard: [
@@ -28,9 +34,7 @@ bot.on('message', async (msg) => {
             }
         })
 
-        await bot.sendMessage('350664322', 'Order')
-
-        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже' + msg.from.id, {
+        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже' + chatId, {
             reply_markup: {
                 inline_keyboard: [
                     [{text: 'Сделать заказ', web_app: {url: webAppUrl}}],
@@ -38,26 +42,23 @@ bot.on('message', async (msg) => {
                 ]
             }
         })
-
-        await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже' + msg.from.id, {
-            reply_markup: {
-                selective: [
-                    [{text: 'Принят'}],
-
-                ]
-            }
-        })
     }
 
-    if(msg?.web_app_data?.data) {
+    if (msg?.web_app_data?.data) {
         try {
             const data = JSON.parse(msg?.web_app_data?.data)
-            console.log(data)
             await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
             await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
             await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
-            await bot.sendMessage('350664322', 'New order! Ваша страна: ' + data?.country + ' Ваша улица: ' + data?.street + msg.from.id)
-
+            await bot.sendMessage(admin_id, 'New order!\nВаша страна: ' + data?.country + '\nВаша улица: ' + data?.street, {
+                reply_markup: {
+                    inline_keyboard: [
+                        [{text: 'Отправлен', callback_data: 'send'}],
+                        [{text: 'Доставлен', callback_data: 'done'}],
+                        [{text: 'Повідомлення', web_app: {url: webAppUrl + '/message'}}]
+                    ]
+                }
+            })
 
             setTimeout(async () => {
                 await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
@@ -66,7 +67,26 @@ bot.on('message', async (msg) => {
             console.log(e);
         }
     }
+
+    bot.on('callback_query', function onCallbackQuery(callbackQuery) {
+        const action = callbackQuery.data;
+        message = callbackQuery.text;
+        if (action === 'send') {
+            bot.sendMessage(user_id, 'Ваше замовлення відправлено');
+        }
+        if (action === 'done') {
+            bot.sendMessage(user_id, 'Ваше замовлення доставленно');
+        }
+        if (action === 'message') {
+            bot.sendMessage(user_id, text);
+        }
+    });
 });
+
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
 
 app.post('/web-data', async (req, res) => {
     const {queryId, products = [], totalPrice} = req.body;
@@ -85,7 +105,4 @@ app.post('/web-data', async (req, res) => {
     }
 })
 
-const PORT = 8000;
-
-app.listen(PORT, () => console.log('server started on PORT ' + PORT))
 
