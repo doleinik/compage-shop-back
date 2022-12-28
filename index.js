234c5ac7bf539aeefaebe17d83f2b106b9046a73
dmitry.oleinik <dmitry.oleinik@rhinocodes.com>
2022-12-28 09:17:59 +0200

    init

--- index.js
+++ index.js
@@ -99,8 +99,10 @@ app.post('/web-data', async (req, res) => {
                 message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
             }
         })
+        res.end();
         return res.status(200).json({});
     } catch (e) {
+        res.end();
         return res.status(500).json({})
     }
 })
